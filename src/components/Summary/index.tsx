bc3f6f1713ddc6cbcc225c4c96a11d5d81d8f290
Denilson Behenck <denilsontrespa10@gmail.com>
2022-09-22 18:54:09 -0300

    sumary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -33,7 +33,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ {summary.income}</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -42,7 +42,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ {summary.outcome}</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -51,7 +51,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ {summary.total}</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
